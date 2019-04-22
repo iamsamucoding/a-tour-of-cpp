220c805244c87728f8b3bf6e86f8c47424fe0d52
Samuel Martins <sbm.martins@gmail.com>
2019-04-22 20:12:25 +0200

    examples of a class (chap 4)

---
+++ 4-classes/complex-numbers/cmake-build-debug/CMakeFiles/tour_cpp_complex.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tour_cpp_complex.dir/main.cpp.o"
+  "CMakeFiles/tour_cpp_complex.dir/complex.cpp.o"
+  "tour_cpp_complex.pdb"
+  "tour_cpp_complex"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tour_cpp_complex.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
