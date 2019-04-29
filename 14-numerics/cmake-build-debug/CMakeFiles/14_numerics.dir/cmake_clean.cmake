fd74574317a4d8e83a9611e140830cd433eefb47
Samuel Martins <sbm.martins@gmail.com>
2019-04-29 19:19:25 +0200

    adding some comments about numerical algorithms

---
+++ 14-numerics/cmake-build-debug/CMakeFiles/14_numerics.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/14_numerics.dir/main.cpp.o"
+  "14_numerics.pdb"
+  "14_numerics"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/14_numerics.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 14-numerics/cmake-build-debug/CMakeFiles/14_numerics.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for 14_numerics.
+# This may be replaced when dependencies are built.
