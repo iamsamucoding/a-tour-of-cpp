20da9af575b5ba4c800372e73a8689db5867d1c0
Samuel Martins <sbm.martins@gmail.com>
2019-04-29 18:38:11 +0200

    adding the most important comments about STL utilities

---
+++ 13-utilities/cmake-build-debug/CMakeFiles/13_utilities.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/13_utilities.dir/main.cpp.o"
+  "13_utilities.pdb"
+  "13_utilities"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/13_utilities.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 13-utilities/cmake-build-debug/CMakeFiles/13_utilities.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for 13_utilities.
+# This may be replaced when dependencies are built.
